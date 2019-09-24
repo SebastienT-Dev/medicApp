b30fee1a6798c97bb471a81a6c4c7a38461d410a
Sebastien Ternisien <sebastienternisien@sebastiens-imac.home>
2019-09-24 19:53:21 +0200

    init commit

---
+++ src/main/java/fr/move/in/med/medicApp/MedicAppApplication.java
@@ -0,0 +1,13 @@
+package fr.move.in.med.medicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicAppApplication.class, args);
+	}
+
+}
