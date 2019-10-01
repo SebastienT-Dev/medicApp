b2f299a78c99508c04694869618dfc22e3e01bd4
Sebastien Ternisien <sebastienternisien@sebastiens-imac.home>
2019-10-01 07:41:59 +0200

    Desactivation de la generation de la bdd + activation du Controller Advice

--- src/main/java/fr/move/in/med/model/Adresse.java
+++ src/main/java/fr/move/in/med/model/Adresse.java
@@ -37,7 +37,13 @@ public class Adresse {
 
 	@Column(name = "adresse")
 	private String adresse;
-
+	
+	@Column(name = "codePostal")
+	private String codePostal;
+	
+	@Column(name = "ville")
+	private String ville;
+	
 	@ManyToOne(cascade = CascadeType.MERGE)
 	@JoinColumn(name = "idTypeAdresse")
 	private TypeAdresse typeAdresse;
@@ -102,6 +108,22 @@ public class Adresse {
 
 	public void setMonPro(Professionnel monPro) {
 		this.monPro = monPro;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
 	}
 
 }
--- src/main/java/fr/move/in/med/vo/AdresseVo.java
+++ src/main/java/fr/move/in/med/vo/AdresseVo.java
@@ -24,6 +24,10 @@ public class AdresseVo {
 
 	private String adresse;
 
+	private String codePostal;
+
+	private String ville;
+
 	private TypeAdresseVo typeAdresse;
 
 	@JsonIgnore(value = true)
@@ -83,6 +87,22 @@ public class AdresseVo {
 
 	public void setMonPro(Professionnel monPro) {
 		this.monPro = monPro;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
 	}
 
 }
