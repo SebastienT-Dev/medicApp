ca0bc0df0dbe76448aa43706f9449f6189f6712b
Sebastien Ternisien <sebastienternisien@sebastiens-imac.home>
2019-10-01 12:46:21 +0200

    Modif Main class pour conf tomcat

--- src/main/java/fr/move/in/med/medicApp/MedicAppApplication.java
+++ src/main/java/fr/move/in/med/medicApp/MedicAppApplication.java
@@ -5,6 +5,8 @@ import org.dozer.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,27 +19,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Configuration
-//@EnableAutoConfiguration
 @EntityScan(basePackages = "fr.move.in.med")
 @ComponentScan(basePackages = "fr.move.in.med")
 @EnableSwagger2
-public class MedicAppApplication {
+public class MedicAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedicAppApplication.class, args);
 	}
-	
+
 	@Bean
 	public Mapper mapper() {
-	    return new DozerBeanMapper();
+		return new DozerBeanMapper();
 	}
-	
+
 	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MedicAppApplication.class);
+	}
 }
